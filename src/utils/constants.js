cccdffd50199b95bb260507dadb5ea3f968aaf2a
Amit Purohit <amitpurohit4747@gmail.com>
2023-04-19 17:21:07 +0530

    changed contract

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./CryptoWill.json";
 
 export const contractABI = abi;
-export const contractAddress = "0xf560AF0690703627456e3Fe4324F3D81ce44F273";
+export const contractAddress = "0xA55d917BBab0c8B5d700eAbCa53B9395f99f15d6";
