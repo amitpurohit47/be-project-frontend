0d2dff2d4b2cebaea196a1a16ae9d345b91c4861
adityaSonawaneTurtlemint <aditya.sonawane@turtlemint.com>
2023-05-15 22:05:11 +0530

    added new contract

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ import abi from "./CryptoWill.json";
 import landAbi from "./LandWill.json";
 
 export const contractABI = abi;
-export const contractAddress = "0xeeB39A26DC5D4766732358335e17BbE29fF0Aa85";
+export const contractAddress = "0xf129D79FD8737725e2c83aC4aBdC29B0f86D82F9";
 
 export const landContractABI = landAbi;
 export const landContractAddress = "0xF0fac94E862E14aAD7A327071bcD81Fb41F98A0D";
